5a52a51a8def52c6b5f04ea7d81fca79dd6f5985
Verity <joshua-schmidt@online.de>
2021-02-24 10:04:32 +0100

    :lock: Enable cors for inceptioncloud.net + enable cookie-parser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express from "express"
 
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
 const app = express()
 const port = 6060
 
+app.use(cors({
+    origin: ['https://inceptioncloud.net'],
+    credentials: true
+}))
+app.use(cookieParser())
+
 app.get("/", (req, res) => {
-    res.send({ status: "success", code: 200} )
+    res.send({ status: "success", code: 200 })
 })
 
 app.listen(port, () => {
