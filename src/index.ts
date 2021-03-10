7ea02c05b5c13d1af12a27d39767cc085af5c6d8
Verity <joshua-schmidt@online.de>
2021-03-10 19:12:42 +0100

    :art: Move /register route to external file

--- src/index.ts
+++ src/index.ts
@@ -5,38 +5,13 @@ require('dotenv/config')
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser")
-const jwt = require("jsonwebtoken")
-const crypto = require("crypto")
-const mongoose = require("mongoose")
 
 const app = express()
 const port = 6060
 
-/*--- MongoDB ---*/
+/*--- Database ---*/
 
-mongoose.connect(`${process.env.DB_STRING}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-
-const database = mongoose.connection
-let accountsCollection = database.collection("accounts")
-let todosCollection = database.collection("todos")
-database.on("error", console.error.bind(console, "connection error:"))
-
-const AccountSchema = mongoose.Schema({
-    username: String,
-    password: String
-})
-
-const TodoSchema = mongoose.Schema({
-    userId: String,
-    todo: String,
-    status: String
-})
-
-const AccountModel = mongoose.model("account", AccountSchema, "accounts")
-const TodoModel = mongoose.model("todos", TodoSchema, "todos")
+connectDatabase()
 
 /*--- Cors & Cookies ---*/
 
@@ -55,58 +30,17 @@ app.get("/", (req, res) => {
 
 /*--- Routes ---*/
 
-app.post("/register", (req, res) => {
-    if(req.body.username != null && req.body.password != null) {
-        const username = req.body.username as string
-        const password = req.body.password as string
+const registerRoute = require("./routes/register.js")
+app.use("/register", registerRoute)
 
-        const pwHash = crypto.createHash("sha1").update(password).digest("hex")
-
-        accountsCollection.findOne({
-            username: username
-        }).then((doc) => {
-            if (doc == null) {
-                const account = new AccountModel({
-                    username: username,
-                    password: pwHash
-                })
-
-                account.save(function (err, doc) {
-                    if (err) return console.error(err)
-
-                    let date = new Date()
-                    date.setDate(date.getDate() + 7)
-
-                    const id = doc._id
-
-                    let token = jwt.sign({
-                        id: id
-                    }, process.env.JWT_SECRET)
-
-                    res.cookie("toshare", token, {
-                        httpOnly: true,
-                        secure: true,
-                        expires: date,
-                        domain: ".inceptioncloud.net"
-                    })
-
-                    res.send({ redirect: "https://inceptioncloud.net/toshare/home" })
-                })
-            } else {
-                res.send({ error: "Username not available!" })
-            }
-        })
-    } else {
-        res.send({ error: "An error occurred! Please try to refresh to page and try again." })
-    }
-})
+/*--- Routes OLD ---*/
 
 app.post("/login", (req, res) => {
     if (req.body.username != null && req.body.password != null) {
         const username = req.body.username as string
         const password = req.body.password as string
 
-        const pwHash = crypto.createHash("sha1").update(password).digest("hex")
+        const pwHash = cryptojs.createHash("sha1").update(password).digest("hex")
 
         accountsCollection.findOne({
             username: username
---
+++ src/routes/register.ts
@@ -0,0 +1,50 @@
+const express = require("express")
+const router = express.Router()
+const cryptojs = require("crypto")
+const jwt = require("jsonwebtoken")
+
+router.post("/register", (req, res) => {
+    if(req.body.username != null && req.body.password != null) {
+        const username = req.body.username as string
+        const password = req.body.password as string
+
+        const pwHash = cryptojs.createHash("sha1").update(password).digest("hex")
+
+        accountsCollection.findOne({
+            username: username
+        }).then((doc) => {
+            if (doc == null) {
+                const account = new AccountModel({
+                    username: username,
+                    password: pwHash
+                })
+
+                account.save(function (err, doc) {
+                    if (err) return console.error(err)
+
+                    let date = new Date()
+                    date.setDate(date.getDate() + 7)
+
+                    const id = doc._id
+
+                    let token = jwt.sign({
+                        id: id
+                    }, process.env.JWT_SECRET)
+
+                    res.cookie("toshare", token, {
+                        httpOnly: true,
+                        secure: true,
+                        expires: date,
+                        domain: ".inceptioncloud.net"
+                    })
+
+                    res.send({ redirect: "https://inceptioncloud.net/toshare/home" })
+                })
+            } else {
+                res.send({ error: "Username not available!" })
+            }
+        })
+    } else {
+        res.send({ error: "An error occurred! Please try to refresh to page and try again." })
+    }
+})
