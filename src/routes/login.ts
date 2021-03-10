0888f47b2c52b738f9d29cae6a192bcf2a0eb391
Verity <joshua-schmidt@online.de>
2021-03-10 22:03:20 +0100

    :sparkles: Convert login-route to external file

---
+++ src/routes/login.ts
@@ -0,0 +1,52 @@
+import express from "express";
+const router = express.Router()
+
+const cryptojs = require("crypto")
+const jwt = require("jsonwebtoken")
+
+router.post("/login", (req, res) => {
+    if (req.body.username != null && req.body.password != null) {
+        const username = req.body.username as string
+        const password = req.body.password as string
+
+        const pwHash = cryptojs.createHash("sha1").update(password).digest("hex")
+
+        accountsCollection.findOne({
+            username: username
+        }).then((doc) => {
+            if (doc != null) {
+
+                const dbPwHash = doc.password
+
+                if (dbPwHash == pwHash) {
+
+                    const dbId = doc._id
+
+                    let date = new Date()
+                    date.setDate(date.getDate() + 7)
+
+                    let token = jwt.sign({
+                        id: dbId
+                    }, process.env.JWT_SECRET)
+
+                    res.cookie("toshare", token, {
+                        httpOnly: true,
+                        secure: true,
+                        expires: date,
+                        domain: ".inceptioncloud.net"
+                    })
+
+                    res.send({ redirect: "https://inceptioncloud.net/toshare/home" })
+
+                } else {
+                    res.send({ error: "Wrong username or password!" })
+                }
+
+            } else {
+                res.send({ error: "Wrong username or password!" })
+            }
+        })
+    } else {
+        res.send({ error: "An error occurred! Please try to refresh to page and try again." })
+    }
+})
